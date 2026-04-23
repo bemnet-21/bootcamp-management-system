f41dea41f114e32f5998f73bfd9891e4f12b96c9
kanzedin <kanzedinakmel0@gmail.com>
2026-04-23 15:47:01 +0300

    Revert "fix(server): remove stale student task/submission routes"
    This reverts commit 7a0aa43c611706fa0439e8a15897853889d1d073.

--- bootcamp-management-system/server/index.js
+++ bootcamp-management-system/server/index.js
@@ -12,6 +12,8 @@ import sessionRoutes from './routes/session.routes.js'
 import attendanceRoutes from './routes/attendance.routes.js'
 import adminBootcampRoutes from './routes/adminBootcamp.routes.js'
 import taskRoutes from './routes/task.routes.js'
+import studentTaskRoutes from './routes/studentTask.routes.js'
+import studentSubmissionRoutes from './routes/studentSubmission.routes.js'
 import instructorRoutes from "./routes/instructor.routes.js";
 import groupsRoute from "./routes/groups.routes.js";
 import gradingRoutes from "./routes/grading.routes.js";
@@ -39,6 +41,8 @@ app.use('/bootcamps/:bootcampId/resources', resourceRoutes)
 app.use("/admin/divisions", divisionRoutes);
 app.use('/admin/bootcamps', adminBootcampRoutes)
 app.use('/bootcamps/:bootcampId/tasks', taskRoutes)
+app.use('/student/tasks', studentTaskRoutes)
+app.use('/student/submissions', studentSubmissionRoutes)
 app.use("/instructor/bootcamps", instructorRoutes);
 app.use("/bootcamps/groups" , groupsRoute);
 app.use("/instructor/submissions/", gradingRoutes)

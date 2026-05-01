0375925a2231e917947588e7aad5d7d6d82d8a08
Amir Ali <amiraliyimer@gmail.com>
2026-05-01 10:16:06 +0300

    adding deactivate and improve login service

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,18 @@
-import express from 'express';
-import { changePassword, login, logout, profile, refreshToken, resetConfirm, resetRequest, updateProfile } from '../controllers/auth.controller.js';
-import protect from '../middlewares/auth.js';
-import { updateUserNotificationPreference } from '../controllers/notifications.controller.js';
+import express from "express";
+import {
+  changePassword,
+  login,
+  logout,
+  profile,
+  refreshToken,
+  resetConfirm,
+  resetRequest,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import protect from "../middlewares/auth.js";
+import { updateUserNotificationPreference } from "../controllers/notifications.controller.js";
 
 const router = express.Router();
-
 
 /**
  * @swagger
@@ -263,17 +271,19 @@ const router = express.Router();
  *         description: User not found
  */
 
+router.post("/login", login);
+router.post("/refresh", refreshToken);
+router.post("/password-reset-request", resetRequest);
+router.post("/password-reset-confirm", resetConfirm);
+router.post("/change-password", protect, changePassword);
+router.get("/me", protect, profile);
+router.put("/me", protect, updateProfile);
+router.post("/logout", protect, logout);
 
-router.post('/login', login)
-router.post('/refresh', protect, refreshToken)
-router.post('/password-reset-request', resetRequest)
-router.post('/password-reset-confirm', resetConfirm)
-router.post('/change-password', protect, changePassword)
-router.get('/me', protect, profile)
-router.put('/me', protect, updateProfile)
-router.post('/logout', protect, logout)
-
-router.put('/me/notification-preferences', protect, updateUserNotificationPreference)
-
+router.put(
+  "/me/notification-preferences",
+  protect,
+  updateUserNotificationPreference,
+);
 
 export default router;
